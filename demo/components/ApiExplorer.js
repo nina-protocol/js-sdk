2fdb9b12005957fd2ec7d87856c433e1231220c0
facing-n <mike@nina.market>
2022-09-26 21:29:13 -0400

    cleanup

--- demo/components/ApiExplorer.js
+++ demo/components/ApiExplorer.js
@@ -131,107 +131,100 @@ const ApiExplorer = () => {
   return (
     <div name='api_explorer' id="#api_explorer" className='flex flex-col flex-grow h-screen font-mono mt-10'>
       <h2 className='mt-2 text-2xl underline uppercase'>Nina API Explorer</h2> 
-      <div className='flex items-end mt-2'>
-        <a className='link' target='_blank' rel="noreferrer"  href='https://nina-protocol.github.io/nina-indexer/'>Docs</a>
-        <a className='ml-2 link' target='_blank' rel="noreferrer" href='https://github.com/nina-protocol/nina-indexer'>
-          Github
-        </a>
-      </div>
-        <ul>
-          <p className='mt-2'>- Select a Resource and click Fetch to retrieve data from the API. Provide a publicKey or modifier to retrieve specific data.</p>
-          <p className='mt-2'>- `With Account Data` returns full formatted on-chain account data.</p>
-        </ul>
+      <ul>
+        <p className='mt-2'>- Select a Resource and click Fetch to retrieve data from the API. Provide a publicKey or modifier to retrieve specific data.</p>
+        <p className='mt-2'>- `With Account Data` returns full formatted on-chain account data.</p>
+      </ul>
 
-        <div className='w-full mt-2'>
-          <form className='pt-2 text-xl'>
-            <div className='flex justify-between gap-4'>
-              <select className='w-1/3 p-2 text-center capitalize border-2 select select-bordered'
-                name="resource"
-                id="resource"
-                value={resource || 'Resource'}
-                onChange={(e) => {
-                  setPublicKey('');
-                  setResource(e.target.value)
-                }}>
-                <option value="none" defaultValue hidden >Resource</option>
-                {Object.keys(resourceOptions).map(resource => (
-                  <option key={resource} value={resource}>{resource}</option>
-                ))}
-              </select>
+      <div className='w-full mt-2'>
+        <form className='pt-2 text-xl'>
+          <div className='flex justify-between gap-4'>
+            <select className='w-1/3 p-2 text-center capitalize border-2 select select-bordered'
+              name="resource"
+              id="resource"
+              value={resource || 'Resource'}
+              onChange={(e) => {
+                setPublicKey('');
+                setResource(e.target.value)
+              }}>
+              <option value="none" defaultValue hidden >Resource</option>
+              {Object.keys(resourceOptions).map(resource => (
+                <option key={resource} value={resource}>{resource}</option>
+              ))}
+            </select>
 
-              <input
-                value={publicKey}
-                disabled={!resource}
-                className='w-1/3 p-2 text-center transition-all duration-200 border-2 input-bordered input '
-                placeholder={resource !== 'Search' ? (resource === 'Hubs' ? 'publicKey or hubHandle' : 'publicKey') : 'query'}
-                type="text"
-                name="publicKey"
-                id="publicKey"
-                onChange={(e) => {
-                  if (e.target.value.length === 0) {
-                    setModifier(undefined)
-                  }
-                  if (e.target.value.indexOf('"') > -1) {
-                    e.target.value = e.target.value.replace(/"/g, '')
-                  }
-                  setPublicKey(e.target.value)
-                }} />
+            <input
+              value={publicKey}
+              disabled={!resource}
+              className='w-1/3 p-2 text-center transition-all duration-200 border-2 input-bordered input '
+              placeholder={resource !== 'Search' ? (resource === 'Hubs' ? 'publicKey or hubHandle' : 'publicKey') : 'query'}
+              type="text"
+              name="publicKey"
+              id="publicKey"
+              onChange={(e) => {
+                if (e.target.value.length === 0) {
+                  setModifier(undefined)
+                }
+                if (e.target.value.indexOf('"') > -1) {
+                  e.target.value = e.target.value.replace(/"/g, '')
+                }
+                setPublicKey(e.target.value)
+              }} />
 
-              <select className='w-1/3 p-2 text-center capitalize transition-all duration-200 border-2 select select-bordered '
-                name="modifier"
-                id="modifier"
-                disabled={!publicKey || publicKey.length === 0 || resource === 'search'}
-                value={modifier || 'Modifier'}
-                onChange={(e) => {
-                  setModifier(e.target.value)
-                }}>
-                <option value="none" defaultValue hidden>Modifier</option>
-                {resourceOptions[resource]?.modifiers.map(modifier => (
-                  <option key={modifier} value={modifier} >{modifier}</option>
-                ))}
-              </select>
-            </div>
+            <select className='w-1/3 p-2 text-center capitalize transition-all duration-200 border-2 select select-bordered '
+              name="modifier"
+              id="modifier"
+              disabled={!publicKey || publicKey.length === 0 || resource === 'search'}
+              value={modifier || 'Modifier'}
+              onChange={(e) => {
+                setModifier(e.target.value)
+              }}>
+              <option value="none" defaultValue hidden>Modifier</option>
+              {resourceOptions[resource]?.modifiers.map(modifier => (
+                <option key={modifier} value={modifier} >{modifier}</option>
+              ))}
+            </select>
+          </div>
 
-            <div className='flex flex-col items-center justify-between gap-4 mt-4 sm:gap-8 sm:flex-row align-center'>
-              <button className='w-full sm:w-[calc(66.666667%-1rem)] p-2 transition-all duration-200 border-2 w-5/8 btn-outline btn capitalize' type="submit"
-                disabled={!resource}
-                onClick={(e) => handleSubmit(e)}>
-                {!resource && 'Select a Resource to explore our API'}
-                {resource && 'Fetch'}
-              </button>
+          <div className='flex flex-col items-center justify-between gap-4 mt-4 sm:gap-8 sm:flex-row align-center'>
+            <button className='w-full sm:w-[calc(66.666667%-1rem)] p-2 transition-all duration-200 border-2 w-5/8 btn-outline btn capitalize' type="submit"
+              disabled={!resource}
+              onClick={(e) => handleSubmit(e)}>
+              {!resource && 'Select a Resource to explore our API'}
+              {resource && 'Fetch'}
+            </button>
 
-              <div className='w-full sm:w-1/3'>
-                <div className='flex justify-end'>
-                  <div className="flex items-center px-2 ml-4 ">
-                    <input id="withAccountData" className='checkbox' type="checkbox" value=""
-                      disabled={!resource || resource === 'search'}
-                      ref={withAccountDataRef}
-                      onChange={(e) => {
-                        setWithAccountData(e.target.checked)
-                      }
-                      }
-                    />
-                    <label htmlFor="withAccountData" className={`ml-2 text-sm ${!resource === true ? 'text-slate-700' : ''} `}>With Account Data</label>
-                  </div>
-
-          
-                  <button className={`py-2 ml-2 btn ${!resource == true || resource === 'resource' ? 'btn-disabled' : 'btn-outlined-default'} `}
-                    disabled={!resource}
-                    onClick={e => handleReset(e)}
-                  >
-                    Reset
-                  </button>
+            <div className='w-full sm:w-1/3'>
+              <div className='flex justify-end'>
+                <div className="flex items-center px-2 ml-4 ">
+                  <input id="withAccountData" className='checkbox' type="checkbox" value=""
+                    disabled={!resource || resource === 'search'}
+                    ref={withAccountDataRef}
+                    onChange={(e) => {
+                      setWithAccountData(e.target.checked)
+                    }
+                    }
+                  />
+                  <label htmlFor="withAccountData" className={`ml-2 text-sm ${!resource === true ? 'text-slate-700' : ''} `}>With Account Data</label>
                 </div>
+
+        
+                <button className={`py-2 ml-2 btn ${!resource == true || resource === 'resource' ? 'btn-disabled' : 'btn-outlined-default'} `}
+                  disabled={!resource}
+                  onClick={e => handleReset(e)}
+                >
+                  Reset
+                </button>
               </div>
-
             </div>
-          </form>
-        </div>
 
+          </div>
+        </form>
+      </div>
 
-        <div className='min-h-[400px] h-full mt-4 overflow-y-auto overflow-x-hidden border-2 mb-4'>
-          <ReactJson src={response} theme='ashes' style={{padding: '8px', minHeight: '100%'}} />
-        </div>
+      <div className='min-h-[400px] h-full mt-4 overflow-y-auto overflow-x-hidden border-2 mb-4'>
+        <ReactJson src={response} theme='ashes' style={{padding: '8px', minHeight: '100%'}} />
+      </div>
     </div>
   )
 }
