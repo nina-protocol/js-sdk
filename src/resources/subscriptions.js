f313e9efba0c559177d8e93f427fa9439d641a8d
eric.j.farber@gmail.com <eric.j.farber@gmail.com>
2022-10-18 12:30:27 -0400

    update subscription comments

--- src/resources/subscriptions.js
+++ src/resources/subscriptions.js
@@ -9,7 +9,7 @@ import NinaClient from '../client';
  * @description Fetches all Posts.
  * @param {Object} [pagination = {limit: 20, offset: 0, sort: 'desc'}] Pagination options.
  * @param {Boolean} [withAccountData = false] Fetch full on-chain Post accounts.
- * @example const subscriptions = await NinaClient.Post.fetchAll();
+ * @example const subscriptions = await NinaClient.Subscription.fetchAll();
  */
 const fetchAll = async (pagination = {}, withAccountData = false) => {
   const { limit, offset, sort } = pagination;
@@ -29,7 +29,7 @@ const fetchAll = async (pagination = {}, withAccountData = false) => {
  * @description Fetches a Post.
  * @param {String} publicKey - The public key of the Post.
  * @param {Boolean} [withAccountData = false] Fetch full on-chain Post account.
- * @example const subscriptions = await NinaClient.Post.fetch("K8XJr7LHWJeJJARTvnsFZViqxBzyDSjsfpS6iBuWhrV");
+ * @example const subscriptions = await NinaClient.Subscription.fetch("K8XJr7LHWJeJJARTvnsFZViqxBzyDSjsfpS6iBuWhrV");
  */
 const fetch = async (publicKey, withAccountData = false, transactionId = undefined) => {
   return NinaClient.get(`/subscriptions/${publicKey}`, transactionId ? { transactionId } : undefined, withAccountData);
